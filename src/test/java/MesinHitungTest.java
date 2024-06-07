3a08e92ae0c38003f47611ee864e57c0faa7a8c2
farhangold <121116261+farhangold@users.noreply.github.com>
2024-06-07 10:27:27 +0700

    Testing Berhasil

--- src/test/java/MesinHitungTest.java
+++ src/test/java/MesinHitungTest.java
@@ -8,25 +8,25 @@ public class MesinHitungTest {
 
     @Test
     public void testPenjumlahan() {
-        assertEquals(5.0, Mesin_Hitung.penjumlahan(2.0, 3.0), 0.001);
+        assertEquals(6.0, Mesin_Hitung.penjumlahan(3.0, 3.0), 0.001);
         assertEquals(-1.0, Mesin_Hitung.penjumlahan(-2.0, 1.0), 0.001);
     }
 
     @Test
     public void testPengurangan() {
-        assertEquals(1.0, Mesin_Hitung.pengurangan(3.0, 2.0), 0.001);
+        assertEquals(2.0, Mesin_Hitung.pengurangan(4.0, 2.0), 0.001);
         assertEquals(-3.0, Mesin_Hitung.pengurangan(-1.0, 2.0), 0.001);
     }
 
     @Test
     public void testPerkalian() {
-        assertEquals(6.0, Mesin_Hitung.perkalian(2.0, 3.0), 0.001);
+        assertEquals(9.0, Mesin_Hitung.perkalian(3.0, 3.0), 0.001);
         assertEquals(-4.0, Mesin_Hitung.perkalian(-2.0, 2.0), 0.001);
     }
 
     @Test
     public void testPembagian() {
-        assertEquals(2.0, Mesin_Hitung.pembagian(6.0, 3.0), 0.001);
+        assertEquals(3.0, Mesin_Hitung.pembagian(9.0, 3.0), 0.001);
         assertEquals(-2.5, Mesin_Hitung.pembagian(-5.0, 2.0), 0.001);
     }
 
